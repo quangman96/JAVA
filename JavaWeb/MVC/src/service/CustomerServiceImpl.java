7d63e63868688611a2c951febbad304e6d069441
quangman96 <54864521+quangman96@users.noreply.github.com>
2019-11-07 15:21:51 +0700

    java

---
+++ JavaWeb/MVC/src/service/CustomerServiceImpl.java
@@ -0,0 +1,48 @@
+package service;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServiceImpl implements CustomerService{
+    private static Map<Integer, Customer> customers;
+
+    static {
+        customers = new HashMap<>();
+        customers.put(1,new Customer(1,"Man","man@gmail.com","Hue"));
+        customers.put(2,new Customer(2,"Luan","luan@gmail.com","Ha noi"));
+        customers.put(3,new Customer(3,"Thanh","thanh@gmail.com","Sai gon"));
+        customers.put(4,new Customer(4,"Nhi","nhi@gmail.com","Da Nang"));
+        customers.put(5,new Customer(5,"Cuong","cuong@gmail.com","Quang Nam"));
+        customers.put(6,new Customer(6,"Phuc","phuc@gmail.com","Nha Trang"));
+
+    }
+
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.put(customer.getId(), customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        customers.put(id, customer);
+    }
+
+    @Override
+    public void remove(int id) {
+        customers.remove(id);
+    }
+}

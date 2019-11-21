275a66cff325065926be90b80a0ad979c71032cc
quangman96 <man.meo.69@gmail.com>
2019-11-21 16:37:42 +0700

    java web

--- JavaWeb/inventory-management-DEV/src/service/DetailImportOrderService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.DetailImportOrder;
-
-import java.util.List;
-
-public interface DetailImportOrderService {
-    List<DetailImportOrder> findByIDImportOrder (int idImportOrder);
-}

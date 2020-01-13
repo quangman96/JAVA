85cd986c8be849a82428d3d0a9de4ffe9ac8339b
quangman96 <man.meo.69@gmail.com>
2020-01-13 14:08:59 +0700

    Done Security

---
+++ JavaSpringBoot/Spring-Shopping/src/main/java/com/codegym/cms/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.codegym.cms;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("man").password(passwordEncoder().encode("231")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("231")).roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/admin/list-product")
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("logout"));
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
